2735598a33f33dc5e90465df8f541f5a81298136
ElenaKushchenko <e.kushchenko@rbkmoney.com>
2021-06-18 10:52:32 +0300

    fix tests

--- src/main/kotlin/com/rbkmoney/claimmanagementapi/config/properties/BouncerProperties.kt
+++ src/main/kotlin/com/rbkmoney/claimmanagementapi/config/properties/BouncerProperties.kt
@@ -2,12 +2,10 @@ package com.rbkmoney.claimmanagementapi.config.properties
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
-import org.springframework.context.annotation.Configuration
 import org.springframework.validation.annotation.Validated
 import javax.validation.constraints.NotEmpty
 
 @Validated
-@Configuration
 @ConstructorBinding
 @ConfigurationProperties(prefix = "bouncer")
 data class BouncerProperties(
